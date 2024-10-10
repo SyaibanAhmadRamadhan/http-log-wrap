a0425455acc7e8391c1681c93b1d2f9682b8f7b0
SyaibanAhmadRamadhan <ibanrama29@gmail.com>
2024-10-10 22:32:04 +0700

    delete custom validator

--- validator.go
+++ validator.go
@@ -12,27 +12,15 @@ func NewValidator() *validator.Validate {
 		return field.Tag.Get("json")
 	})
 
-	v.RegisterValidation("required_if", requiredIf)
+	//v.RegisterValidation("required_if", requiredIf)
 	return v
 }
 
 func registerTranslations(validate *validator.Validate, trans ut.Translator) {
-	validate.RegisterTranslation("required_if", trans, func(ut ut.Translator) error {
-		return ut.Add("required_if", "{0} must be filled if {1} has a value.", true)
-	}, func(ut ut.Translator, fe validator.FieldError) string {
-		t, _ := ut.T("required_if", fe.Field(), fe.Param())
-		return t
-	})
-}
-
-func requiredIf(fl validator.FieldLevel) bool {
-	fieldName := fl.Param()
-	fieldValue := fl.Field().String()
-
-	otherFieldValue := fl.Parent().FieldByName(fieldName).String()
-
-	if otherFieldValue != "" && fieldValue == "" {
-		return false
-	}
-	return true
+	//validate.RegisterTranslation("required_if", trans, func(ut ut.Translator) error {
+	//	return ut.Add("required_if", "{0} must be filled if {1} has a value.", true)
+	//}, func(ut ut.Translator, fe validator.FieldError) string {
+	//	t, _ := ut.T("required_if", fe.Field(), fe.Param())
+	//	return t
+	//})
 }
